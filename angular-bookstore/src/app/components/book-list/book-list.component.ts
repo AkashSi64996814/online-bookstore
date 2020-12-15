49f815939754944a9fe0f01c13919ae9a2002283
DELL <DELL@DELL-PC>
2020-12-15 16:40:23 +0530

    created angular project and completed the feature 1.

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Book} from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
+
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[]
+  constructor(private _bookServive : BookService) { }
+
+  ngOnInit(){ 
+    this.listBooks();
+  }
+
+  listBooks(){
+    this._bookServive.getBooks().subscribe(
+      data => {
+        this.books = data
+      }
+    )
+  }
+
+}
--- online-bookstore/src/main/java/in/akashsingh/repository/BookRepository.java
+++ online-bookstore/src/main/java/in/akashsingh/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package in.akashsingh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.akashsingh.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
